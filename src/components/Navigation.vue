f63f9a10717ff7feebecfc968776a29bb52e1854
Hazy <Wchodz3WTo@gmail.com>
2024-10-22 07:34:44 +0200

    UI adjuest

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,16 +1,14 @@
 <template>
-  <div class="flex flex-col lg:flex-row">
-    <div class="grid grid-cols-2">
-      <button
-        v-for="link in ['tracker', 'share']"
-        :key="link"
-        @click="tracker = link"
-        class="nav-btn"
-        :class="{ active: tracker === link }"
-      >
-        {{ link }}
-      </button>
-    </div>
+  <div class="grid grid-cols-2">
+    <button
+      v-for="link in ['tracker', 'share']"
+      :key="link"
+      @click="tracker = link"
+      class="nav-btn"
+      :class="{ active: tracker === link }"
+    >
+      {{ link }}
+    </button>
   </div>
 </template>
 
